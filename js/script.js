ed1c095a2cb7750ddfd28be978a3dcbe35c04962
mikedsousa <95877997+mikedsousa@users.noreply.github.com>
2024-01-10 08:53:59 -0300

    password generator done

--- js/script.js
+++ js/script.js
@@ -13,18 +13,54 @@ const getLetterUpperCase = () => {
   return String.fromCharCode(Math.floor(Math.random() * 26) + 65);
 };
 
-const getNumber =() => {
+const getNumber = () => {
   return Math.floor(Math.random() * 10).toString();
-}
+};
 
 const getSymbol = () => {
   const symbols = "!@#$%&*()_+{}[],.?/|-";
   return symbols[Math.floor(Math.random() * symbols.length)];
-}
+};
 
+const generatePassword = (
+  getLetterLowerCase,
+  getLetterUpperCase,
+  getNumber,
+  getSymbol
+) => {
+  let password = "";
+
+  const passwordLength = 10;
+
+  const generators = [
+    getLetterLowerCase,
+    getLetterUpperCase,
+    getNumber,
+    getSymbol,
+  ];
+
+  for (let i = 0; i < passwordLength; i = i + 4) {
+    generators.forEach(() => {
+      const randomValue =
+        generators[Math.floor(Math.random() * generators.length)]();
+
+      password += randomValue;
+    });
+  }
+
+  password = password.slice(0, passwordLength);
+
+  generetedPasswordElement.style.display = "block";
+  generetedPasswordElement.querySelector("h4").innerText = password;
+};
 
 // Eventos
 
 generatePasswordButton.addEventListener("click", () => {
-  console.log("olá");
+  generatePassword(
+    getLetterLowerCase,
+    getLetterUpperCase,
+    getNumber,
+    getSymbol
+  );
 });
