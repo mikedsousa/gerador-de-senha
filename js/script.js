a7341828e897b05af2f69475c44796e3af6d04d7
mikedsousa <95877997+mikedsousa@users.noreply.github.com>
2024-01-10 08:09:24 -0300

    functions done

---
+++ js/script.js
@@ -0,0 +1,30 @@
+// Seleção de elementos
+
+const generatePasswordButton = document.querySelector("#generate-password");
+const generetedPasswordElement = document.querySelector("#generated-password");
+
+// Funções
+
+const getLetterLowerCase = () => {
+  return String.fromCharCode(Math.floor(Math.random() * 26) + 97);
+};
+
+const getLetterUpperCase = () => {
+  return String.fromCharCode(Math.floor(Math.random() * 26) + 65);
+};
+
+const getNumber =() => {
+  return Math.floor(Math.random() * 10).toString();
+}
+
+const getSymbol = () => {
+  const symbols = "!@#$%&*()_+{}[],.?/|-";
+  return symbols[Math.floor(Math.random() * symbols.length)];
+}
+
+
+// Eventos
+
+generatePasswordButton.addEventListener("click", () => {
+  console.log("olá");
+});
